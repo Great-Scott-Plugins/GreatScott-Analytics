b3a19594c40f65c037e4c6e3bd91ebc289b3548e
Scott Weaver <scottmweaver@gmail.com>
2025-01-18 14:41:17 -0800

    (chore) Add GreatScott/WordPress plugin dependency

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'great-scott-plugins/greatscott-analytics',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'great-scott-plugins/greatscott-analytics' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'great-scott-plugins/wordpress-plugin' => array(
+            'pretty_version' => '1.0.4',
+            'version' => '1.0.4.0',
+            'reference' => 'f547a82fed8c94d2daecbf3b2656fd5f1505495f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../great-scott-plugins/wordpress-plugin',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
